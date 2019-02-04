9670135db6ce6729404a1e5a480c2d28e23fef20
Simen Bekkhus <sbekkhus91@gmail.com>
2019-02-04 14:11:29 +0100

    update to jest 24

--- examples/typescript/jest.config.js
+++ examples/typescript/jest.config.js
@@ -2,22 +2,15 @@ module.exports = {
   projects: [
     {
       displayName: 'test',
-      moduleFileExtensions: ['ts', 'js'],
-      testMatch: ['<rootDir>/**/__tests__/**/*.test.(j|t)s'],
-      transform: {
-        '^.+\\.(j|t)s$': 'babel-jest',
-      },
     },
     {
-      runner: 'jest-runner-tslint',
-      displayName: 'tslint',
-      moduleFileExtensions: ['ts'],
+      runner: 'jest-runner-eslint',
+      displayName: 'eslint',
       testMatch: ['<rootDir>/**/*.ts'],
     },
     {
       runner: 'jest-runner-tsc',
       displayName: 'tsc',
-      moduleFileExtensions: ['ts', 'tsx'],
       testMatch: ['<rootDir>/**/*.ts'],
     },
   ],
